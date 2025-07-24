ab53aed6cf2f6a13d20291f132c7f51dace68fab
followzoa <followzoa@gmail.com>
2025-07-24 14:27:03 +0900

    [frontend] core: 웹폰트 설정 파일 추가

---
+++ frontend/src/shared/config/theme/fonts.ts
@@ -0,0 +1,58 @@
+import { css } from 'styled-components';
+
+export const fontFaces = css`
+    @font-face {
+        font-family: 'SUIT-Regular';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Regular.woff2') format('woff2');
+    }
+
+    @font-face {
+        font-family: 'SUIT-Medium';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Medium.woff2') format('woff2');
+    }
+
+    @font-face {
+        font-family: 'SUIT-SemiBold';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-SemiBold.woff2') format('woff2');
+    }
+
+    @font-face {
+        font-family: 'SUIT-Bold';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Bold.woff2') format('woff2');
+    }
+
+    @font-face {
+        font-family: 'SUIT-ExtraBold';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-ExtraBold.woff2') format('woff2');
+    }
+
+    @font-face {
+        font-family: 'SUIT-Heavy';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Heavy.woff2') format('woff2');
+    }
+
+    @font-face {
+        font-family: 'Paperlogy-4Regular';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2408-3@1.0/Paperlogy-4Regular.woff2') format('woff2');
+    }
+
+    @font-face {
+        font-family: 'Paperlogy-6SemiBold';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2408-3@1.0/Paperlogy-6SemiBold.woff2') format('woff2');
+    }
+
+    @font-face {
+        font-family: 'Paperlogy-8ExtraBold';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2408-3@1.0/Paperlogy-8ExtraBold.woff2') format('woff2');
+    }
+
+    @font-face {
+        font-family: 'Paperlogy-9Black';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2408-3@1.0/Paperlogy-9Black.woff2') format('woff2');
+    }
+
+    @font-face {
+        font-family: 'Pretendard-Regular';
+        src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts[[210]]7@1.1/Pretendard-Regular.woff') format('woff');
+    }
+`;
