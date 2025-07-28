4dc05e55b01dc406f6d99b4e7c98aba4ba6b0de4
followzoa <followzoa@gmail.com>
2025-07-28 18:07:13 +0900

    [frontend] chore: appConfig 설정

--- frontend/src/@types.ts
+++ frontend/src/@types.ts
@@ -1,7 +1,17 @@
 import type { ThemeT } from "@/shared";
 
 declare module 'styled-components' {
-
     export interface DefaultTheme extends ThemeT {
+    }
+}
+
+interface AppConfig {
+    APP_NAME: string;
+    BASE_API_URL: string;
+}
+
+declare global {
+    interface Window {
+        __APP_CONFIG__: AppConfig;
     }
 }
