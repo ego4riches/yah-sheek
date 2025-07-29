9a6856bfbe6484c350759e5bfdb5af6984f980b7
followzoa <followzoa@gmail.com>
2025-07-29 16:12:47 +0900

    [frontend] chore: review-filter-bar 삭제

--- frontend/src/widgets/review-filter-bar/config/styled.ts
+++
@@ -1,9 +0,0 @@
-import { flexSpaceBetween } from "@/shared";
-import styled from "styled-components";
-
-export const ReviewFilterBarWrapper = styled.div`
-    ${ flexSpaceBetween };
-    padding: 2rem;
-    background: ${ ({ theme }) => theme.colors.ssg };
-    border-radius: ${ ({ theme }) => theme.borderRadius.xl };
-`
