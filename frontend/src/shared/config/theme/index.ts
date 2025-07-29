bd55ab30292f937bb9b773766b63a00486802ef2
followzoa <followzoa@gmail.com>
2025-07-29 17:51:14 +0900

    [frontend] feat: 반응형 미디어 쿼리 정의

--- frontend/src/shared/config/theme/index.ts
+++ frontend/src/shared/config/theme/index.ts
@@ -11,5 +11,6 @@ export const theme = {
 export type ThemeT = typeof theme;
 
 export * from '@/shared/config/theme/fonts';
+export * from '@/shared/config/theme/media';
 export * from '@/shared/config/theme/tokens';
 export * from '@/shared/config/theme/utils';
