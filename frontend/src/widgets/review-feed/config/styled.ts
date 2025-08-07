1582d258e9b76d304aa0c5dcd319c2658ef3793c
followzoa <followzoa@gmail.com>
2025-08-07 17:27:31 +0900

    [frontend] style: 미사용 스타일 코드 제거

--- frontend/src/widgets/review-feed/config/styled.ts
+++ frontend/src/widgets/review-feed/config/styled.ts
@@ -1,12 +1,6 @@
 import styled from "styled-components";
-// import { getShouldForwardProps } from "@/shared";
-
-// export const styledBox = styled.div.withConfig({
-//     shouldForwardProp: getShouldForwardProps(['']),
-// })<{}>`
-// `
 
 export const ReviewFeedWrapper = styled.div`
     width: 100%;
-    margin-top: 1rem;
+    margin-top: 3rem;
 `
