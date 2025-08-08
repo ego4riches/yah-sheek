0089612e81b50b723aa3e05885960fdfc165c9c1
followzoa <followzoa@gmail.com>
2025-08-08 17:24:26 +0900

    [frontend] feat: 유틸 함수 추가

--- frontend/src/shared/lib/utils/index.ts
+++ frontend/src/shared/lib/utils/index.ts
@@ -3,3 +3,4 @@ export * from '@/shared/lib/utils/get-ball-park-by-team-key';
 export * from '@/shared/lib/utils/get-should-forward-props';
 export * from '@/shared/lib/utils/get-star-rating';
 export * from '@/shared/lib/utils/hex-to-rgba';
+export * from '@/shared/lib/utils/to-key-value-map';
