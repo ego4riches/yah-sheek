95b8fd27e4efecd445a911ac420a74c3a96641fe
followzoa <followzoa@gmail.com>
2025-08-14 16:55:11 +0900

    [frontend] chore: endpoint 추가 환경 설정

--- frontend/src/shared/api/http-client/api-url.constants.ts
+++ frontend/src/shared/api/http-client/api-url.constants.ts
@@ -1,5 +1,11 @@
 // API BASE URL
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+// 카테고리 API BASE URL
+export const CATEGORIES_BASE_URL = import.meta.env.VITE_CATEGORIES_API_PREFIX;
+
 // 리뷰 API BASE URL
 export const REVIEWS_BASE_URL = import.meta.env.VITE_REVIEWS_API_PREFIX;
+
+// 팀 API BASE URL
+export const TEAMS_BASE_URL = import.meta.env.VITE_TEAMS_API_PREFIX;
