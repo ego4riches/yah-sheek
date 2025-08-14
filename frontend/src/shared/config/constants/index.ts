d385a2c595b57188ebbdaea7c73062c81a7f6d10
followzoa <followzoa@gmail.com>
2025-08-14 16:54:52 +0900

    [frontend] feat: teams 메시지 상수 생성

--- frontend/src/shared/config/constants/index.ts
+++ frontend/src/shared/config/constants/index.ts
@@ -1,3 +1,4 @@
+export * from '@/shared/config/constants/messages';
 export * from '@/shared/config/constants/api';
 export * from '@/shared/config/constants/routes';
 export * from '@/shared/config/constants/teams';
---
+++ frontend/src/shared/config/constants/messages/teams.ts
@@ -0,0 +1,4 @@
+export const TEAMS_MESSAGES = {
+    TEAM_NOT_FOUND: '팀 정보를 찾을 수 없습니다.',
+    TEAM_FETCH_ERROR: '팀 정보를 불러오는 중 오류가 발생했습니다.',
+}
