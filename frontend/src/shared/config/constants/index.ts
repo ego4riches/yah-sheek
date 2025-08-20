ded3aaad75977960176a7d2cd2e8d85662990a47
followzoa <followzoa@gmail.com>
2025-08-20 14:39:30 +0900

    [frontend] refactor: 불필요한 유틸 제거 및 export index 추가

--- frontend/src/shared/config/constants/index.ts
+++ frontend/src/shared/config/constants/index.ts
@@ -1,6 +1,5 @@
 export * from '@/shared/config/constants/messages';
 export * from '@/shared/config/constants/api';
 export * from '@/shared/config/constants/routes';
-export * from '@/shared/config/constants/teams';
 export * from '@/shared/config/constants/texts';
 export * from '@/shared/config/constants/ui-text';
