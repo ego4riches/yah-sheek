0bab20b6fd4a4d7795f6e6b5da3eaefc79dae286
followzoa <followzoa@gmail.com>
2025-08-20 14:42:10 +0900

    [frontend] feat: Drawer -> TeamsDrawer 변경 적용

--- frontend/src/widgets/header/ui/Header.tsx
+++ frontend/src/widgets/header/ui/Header.tsx
@@ -1,12 +1,12 @@
-import {AuthButton} from "@/features";
-import {Drawer, ROUTES} from "@/shared";
+import {AuthButton, TeamsDrawer} from "@/features";
+import {ROUTES} from "@/shared";
 import BaseBall from '@/shared/config/assets/baseball.png'
 import {HeaderBox, LogoBox} from "@/widgets";
 
 export const Header = () => {
     return (
             <HeaderBox>
-                <Drawer/>
+                <TeamsDrawer/>
                 <LogoBox to={ROUTES.HOME}>
                     <img src={BaseBall} alt="baseball"/>
                 </LogoBox>
