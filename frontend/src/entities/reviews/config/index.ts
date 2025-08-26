334ab266c67560694bd9593fbc25fedff4747559
followzoa <followzoa@gmail.com>
2025-08-26 16:36:47 +0900

    [frontend] docs: mock-data 제거

--- frontend/src/entities/reviews/config/index.ts
+++ frontend/src/entities/reviews/config/index.ts
@@ -1,4 +1,3 @@
 export * from '@/entities/reviews/config/constants';
-export * from '@/entities/reviews/config/feed.mock-data';
 export * from '@/entities/reviews/config/query-keys';
 export * from '@/entities/reviews/config/styled';
