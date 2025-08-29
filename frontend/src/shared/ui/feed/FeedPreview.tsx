eae31b4e8b5ae0c7a6c24fd5afb50136005d30d1
followzoa <followzoa@gmail.com>
2025-08-29 17:03:07 +0900

    [frontend] feat: 별점 좋아요 변경된 컴포넌트 적용

--- frontend/src/shared/ui/feed/FeedPreview.tsx
+++ frontend/src/shared/ui/feed/FeedPreview.tsx
@@ -1,4 +1,4 @@
-import { type FeedPreviewT, FeedPreviewWrapper, formatDate, getStarRating, PreviewCategoryBox, PreviewContentBox, PreviewMetaBox } from "@/shared";
+import { type FeedPreviewT, FeedPreviewWrapper, formatDate, PreviewCategoryBox, PreviewContentBox, PreviewMetaBox, RatingStar } from "@/shared";
 
 export const FeedPreview = ({ feed }: FeedPreviewT) => {
     const { nickname, categoryId, categoryName, content, createdAt, rating, viewsCount } = feed;
@@ -11,7 +11,7 @@ export const FeedPreview = ({ feed }: FeedPreviewT) => {
                     <span>{nickname}</span>
                     <span>{formatDate(createdAt)}</span>
                     <span>{viewsCount}</span>
-                    <span>{getStarRating(rating)}</span>
+                    <span>{RatingStar(rating)}</span>
                 </PreviewMetaBox>
             </FeedPreviewWrapper>
     );
