f196ac4cc3fc03fdb6ec3e997a20f9ee24fc803b
followzoa <followzoa@gmail.com>
2025-08-29 09:23:23 +0900

    [frontend] feat: Review Detail Viewer 모드가 아닐 경우 Composer 제거

--- frontend/src/widgets/review-composer/ui/ReviewComposer.tsx
+++ frontend/src/widgets/review-composer/ui/ReviewComposer.tsx
@@ -1,17 +1,19 @@
 import { CategoriesSelector } from "@/entities/categories/ui/selector";
 import { useAddReviewForm } from '@/features';
-import { RatingSelector, ReviewContentInput, TagManager } from '@/shared';
+import { RatingSelector, ReviewContentInput, TagManager, useFeedStore } from '@/shared';
 import { MediaUpload } from '@/shared/ui/media-upload/MediaUpload.tsx';
 import { ComposerExpandedWrapper, ComposerHeaderBox, ReviewComposerWrapper, useReviewComposerStore, useReviewComposerVisibility, } from '@/widgets/review-composer';
 
 export const ReviewComposer = () => {
     const { isExpanded, setIsExpanded } = useReviewComposerStore();
+    const { selectedFeedId } = useFeedStore();
     const { isSubmitDisabled, submitReview } = useAddReviewForm();
     const { composerRef } = useReviewComposerVisibility();
 
     const handleFocus = () => setIsExpanded(true);
 
     return (
+            !selectedFeedId &&
             <ReviewComposerWrapper ref={composerRef} isExpanded={isExpanded}>
                 <ComposerExpandedWrapper isExpanded={isExpanded}>
                     <ComposerHeaderBox>
